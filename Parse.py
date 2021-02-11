f9a7e49cc641a50b7fadb1e7cd5f454cfb227fe6
= <=>
2021-02-12 00:11:47 +0500

    Change README and Parse.py

--- Parse.py
+++ Parse.py
@@ -1,28 +1,31 @@
+import time
 import requests
 from bs4 import BeautifulSoup
+import json
+from urllib.parse import urlparse
 
 def parse(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     }
 
+    addPageUrl = ''
+    #r = requests.post()
+
     response = requests.get(url, headers)
+    #soup = BeautifulSoup(response.content, 'html.parser')
+
     soup = BeautifulSoup(response.content, 'html.parser')
+
     items = soup.findAll('div', class_ = 'reviews-view__review')
-    
     rating = ''
     evaluation = 0
-    organizations = []
     comments = []
     
     for item in items:
         evaluationBlock = item.find('div', class_ = 'business-review-view__header').find('div', class_ = 'business-rating-badge-view__stars').findAll('span', class_ = 'business-rating-badge-view__star _size_m')
         for char in evaluationBlock:
             evaluation += 1
-
-        ratingBlock = soup.find('span', class_ = 'business-summary-rating-badge-view__rating').findAll('span', class_ = 'business-summary-rating-badge-view__rating-text')
-        for char in ratingBlock:
-            rating += char.text
 
         comments.append({
             'author': item.find('div', class_ = 'business-review-view__author').find('span').text,
@@ -31,12 +34,16 @@ def parse(url):
             'text': item.find('div', class_ = 'business-review-view__body-text _collapsed').text
         })
 
-        organizations.append({
-            'name': soup.find('h1', class_ = 'orgpage-header-view__header').text,
-            'rating': rating,
-            'comments': comments
-        })
-
         evaluation = 0
+
+    ratingBlock = soup.find('span', class_ = 'business-summary-rating-badge-view__rating').findAll('span', class_ = 'business-summary-rating-badge-view__rating-text')
+    for char in ratingBlock:
+        rating += char.text
+
+    organizations = {
+        'name': soup.find('h1', class_ = 'orgpage-header-view__header').text,
+        'rating': rating,
+        'comments': comments
+    }
 
     return organizations
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import time
 import Parse
+import json
 
 def main():
-    url = 'https://yandex.ru/profile/78003645021'
-    
-    organizations = Parse.parse(url)
+    url = 'https://yandex.ru/maps/org/shartashskiy_lesopark/155882288890/?ll=60.681040%2C56.860309&z=12'
+    organizations = []
+    organizations.append(Parse.parse(url))
     time.sleep(5)
 
-    print(organizations)
+    with open('Data/organizations.json', 'a', encoding='utf-8') as file:
+        json.dump(organizations, file, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
     main()
